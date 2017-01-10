8c867ed6945c38a475655fe95531804f9b38df61
sahil143 <sahilbudhwar143@gmail.com>
2017-01-10 09:58:02 +0530

    Linear Search Python

--- Searching/linear_search/linear_search.py
+++ Searching/linear_search/linear_search.py
@@ -7,8 +7,12 @@ def linear_search(a, b):
 			return True
 
 print("Enter the list:")
+
+# take inputs from user
 a = sys.stdin.readline().split()
 b = input("Enter the number to be searched: ")
+
+# check if the entity is found or not
 if linear_search(a, b) == True:
 	print("found match")
 else:
